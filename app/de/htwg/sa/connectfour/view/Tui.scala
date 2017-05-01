15096573118904c7725e0060fc004b2b2b5ce1e1
danielfranze <daniel.franze.office@gmail.com>
2017-05-01 22:56:01 +0200

    Add tui

---
+++ app/de/htwg/sa/connectfour/view/Tui.scala
@@ -0,0 +1,24 @@
+package de.htwg.sa.connectfour.view
+
+import de.htwg.sa.connectfour.controller.Controller
+import de.htwg.sa.connectfour.model.{Matchfield, Player}
+import de.htwg.sa.connectfour.util.Observer
+
+class Tui(controller: Controller) extends Observer{
+
+  controller.add(this)
+
+  def processInputLine(input: String):Unit = {
+    input match {
+      case "q" =>
+      case "n"=> controller.createEmptyMatchfield()
+      case _ => input.toList.filter(c => c != ' ').map(c => c.toString.toInt) match {
+          case row :: column :: Nil => controller.set(row, column)
+          case _ =>
+        }
+
+    }
+  }
+
+  override def update: Unit =  println(controller.matchfieldToString)
+}
