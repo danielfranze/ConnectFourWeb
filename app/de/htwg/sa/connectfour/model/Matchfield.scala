0aa1fda92beb774674fe736625748388d16ac5d1
danielfranze <daniel.franze.office@gmail.com>
2017-05-01 16:09:49 +0200

    Add toString() in Matchfield

--- app/de/htwg/sa/connectfour/model/Matchfield.scala
+++ app/de/htwg/sa/connectfour/model/Matchfield.scala
@@ -1,23 +1,38 @@
 package de.htwg.sa.connectfour.model
 
-import scala.util.Try
-
 class Matchfield(row: Int, column: Int){
-
-  val matrix = Array.fill[String](row,column)("empty")
-
+  val matrix: Array[Array[String]] = Array.fill[String](row,column)("empty")
 
   def areFourConnected(player: String): Boolean = {
+    val winNumber = 4
+
     def horizontalCheck(): Boolean ={
       var winCounter = 0
+      var `Was the game won?` = false
+
       for(currentRow <- 0 until row) for(currentColumn <- 0 until column){
         if(matrix(currentRow)(currentColumn) == player) winCounter += 1 else winCounter = 0
-        if(winCounter == 4){return true}
+        if(winCounter == winNumber){`Was the game won?` = true}
       }
-      false
+      `Was the game won?`
     }
-    if(horizontalCheck() == true) true else false
+
+    if(horizontalCheck()) true else false
 
   }
+
+  override def toString: String = {
+    var printMatrix = ""
+    for(row <- matrix) {
+      printMatrix += "+" + ("---+" * column) + "\n|"
+      for(column <- row){
+        if(column == "empty") printMatrix += "   |" else printMatrix += (column + " |")
+      }
+      printMatrix += "\n"
+    }
+    printMatrix += "+" + ("---+" * column) + "\n"
+    printMatrix
+  }
+
 
 }
