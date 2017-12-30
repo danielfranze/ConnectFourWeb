757d20f19379a68b48f6c6f427f0ac6ad5d349cd
danielfranze <daniel.franze.office@gmail.com>
2017-12-30 12:27:25 +0100

    Add tests

---
+++ test/de/htwg/sa/connectfour/controller/ControllerSpec.scala
@@ -0,0 +1,29 @@
+package de.htwg.sa.connectfour.controller
+import de.htwg.sa.connectfour.model.{Matchfield, Player}
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ControllerSpec extends WordSpec with Matchers {
+  val (rows, columns) = (6,7)
+  val player1 = Player(1, "player1")
+  val player2 = Player(2, "player2")
+  val test_controller = new Controller(new Matchfield(rows, columns), player1, player2)
+  "The Controller" when {
+    "create a empty Matchfield" should {
+      test_controller.createEmptyMatchfield()
+      "the value" in{
+        test_controller.gameIsWon should be(false)
+      }
+      "the columns" in {
+        var columns_counter = test_controller.matchfield.matrix(rows-1)
+        var sum = 0
+        for (element <- columns_counter){
+          sum += 1
+        }
+        sum should be(columns)
+      }
+    }
+  }
+}
