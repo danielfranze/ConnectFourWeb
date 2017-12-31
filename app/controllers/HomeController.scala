e4c3cc10b5c258f07c9603ff7a026992eaf9e362
danielfranze <daniel.franze.office@gmail.com>
2017-12-31 11:49:39 +0100

    Add JSON

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -5,13 +5,17 @@ import javax.inject._
 
 import play.api._
 import play.api.mvc._
-import play.api.libs.json._
 import java.util.{HashMap, Map}
 
+import com.fasterxml.jackson.databind.JsonNode
 import de.htwg.sa.connectfour.Start.controller
 import de.htwg.sa.connectfour.model.{Matchfield, Player}
+import play.api.libs.json.{JsObject, JsValue, Json}
 
-
+import scala.collection.mutable
+import scala.util.parsing.json.JSONObject
+//import play.libs.Json
+import collection.mutable.HashMap
 
 
 
@@ -23,42 +27,40 @@ class HomeController @Inject() extends Controller {
   val player2 = Player(2, "player2")
   val controller = new de.htwg.sa.connectfour.controller.Controller(new Matchfield(rows, columns), player1, player2)
 
+
+  def matrixToJsonString:mutable.HashMap[String, String] = {
+    val matrixList = new mutable.HashMap[String, String]()
+    for(row <- 0 to 5)
+      for(column <- 0 to 6){
+        matrixList.put(s"$row$column", controller.matchfield.matrix(row)(column))
+      }
+    matrixList
+  }
+
+  def matrixToJson:JsValue = {
+    Json.toJson(matrixToJsonString)
+  }
+
   def index = Action {
-    controller.set(5,0)
-    val test:String = controller.currentPlayer.name
-
+    controller.set(5, 0)
+    val test: String = controller.currentPlayer.name
+    val test1 = matrixToJson
     controller.notifyObservers()
-    Ok(views.html.index(s"Current Player: $test"))
-
-
-
-
-
+    Ok(views.html.index(s"Current Player: $test  $test1"))
+  }
 
 
 
     //var test:Map[String, String] = new HashMap()
     //test.put("id", "Daniel")
-    /*val json: JsValue = Json.parse("""
+    /*val json: JsValue = play.api.libs.json.Json.parse("""
   {
     "name" : "Watership Down",
-    "location" : {
-      "lat" : 51.235685,
-      "long" : -1.309197
-    },
-    "residents" : [ {
-      "name" : "Fiver",
-      "age" : 4,
-      "role" : null
-    }, {
-      "name" : "Bigwig",
-      "age" : 6,
-      "role" : "Owsla"
-    } ]
+    ""
   }
   """)
-    Ok(Json.toJson(json))*/
+    Ok(Json.toJson(json))
 
-  }
+  }*/
 
 }
