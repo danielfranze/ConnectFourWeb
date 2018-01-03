ff96e748ec1ee1c1cf16c2fbb9016b2b0af2bc58
danielfranze <daniel.franze.office@gmail.com>
2018-01-03 03:46:07 +0100

    Fix integration test

--- test/IntegrationSpec.scala
+++ test/IntegrationSpec.scala
@@ -14,7 +14,7 @@ class IntegrationSpec extends PlaySpec with OneServerPerTest with OneBrowserPerT
 
       go to ("http://localhost:" + port)
 
-      pageSource must include ("Current Player:")
+      pageSource must include ("<h1 id=\"player\" style=\"float:none !important;\">")
     }
   }
 }
