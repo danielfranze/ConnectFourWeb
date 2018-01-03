6777ed72aefb88c696ea5e4c672d5063fdff86e1
danielfranze <daniel.franze.office@gmail.com>
2018-01-03 03:28:05 +0100

    Add diagonal check

--- app/de/htwg/sa/connectfour/model/Matchfield.scala
+++ app/de/htwg/sa/connectfour/model/Matchfield.scala
@@ -3,35 +3,78 @@ package de.htwg.sa.connectfour.model
 class Matchfield(row: Int, column: Int){
   val matrix: Array[Array[String]] = Array.fill[String](row,column)("empty")
 
+  val winNumber = 4
+  var lastMoveRow = 0
+  var lastMoveColumn = 0
+
+  def horizontalCheck(player: String) ={
+    var winCounter = 0
+    var `Was the game won?` = false
+
+    for(currentRow <- 0 until row) for(currentColumn <- 0 until column){
+      if(matrix(currentRow)(currentColumn) == player) winCounter += 1 else winCounter = 0
+      if(winCounter == winNumber){`Was the game won?` = true}
+    }
+    `Was the game won?`
+  }
+
+  def verticalCheck(player: String) ={
+    var winCounter = 0
+    var `Was the game won?` = false
+    for(currentColumn <- 0 until column) for(currentRow <- 0 until row){
+      if(matrix(currentRow)(currentColumn) == player) winCounter += 1 else winCounter = 0
+      if(winCounter == winNumber){`Was the game won?` = true}
+    }
+    `Was the game won?`
+  }
+
+  def diagonalCheck(player: String) ={
+    var winCounterRight = 1
+    var winCounterLeft = 1
+    var `Was the game won?` = false
+
+    for(i <- 1 to 3){
+    scala.util.control.Exception.ignoring(classOf[ArrayIndexOutOfBoundsException]) {
+
+      if (matrix(lastMoveRow + i)(lastMoveColumn + i) == player) {
+        winCounterLeft += 1
+      }
+    }
+      scala.util.control.Exception.ignoring(classOf[ArrayIndexOutOfBoundsException]) {
+        if(matrix(lastMoveRow - i)(lastMoveColumn - i) == player){
+          winCounterLeft += 1
+        }}
+      scala.util.control.Exception.ignoring(classOf[ArrayIndexOutOfBoundsException]) {
+        if(matrix(lastMoveRow - i)(lastMoveColumn + i) == player){
+          winCounterRight += 1
+        }}
+      scala.util.control.Exception.ignoring(classOf[ArrayIndexOutOfBoundsException]) {
+        if (matrix(lastMoveRow + i)(lastMoveColumn - i) == player) {
+          winCounterRight += 1
+        }
+      }
+
+    }
+
+    println("Counter left: " + winCounterLeft + " Counter Right: " + winCounterRight)
+
+
+    if(winCounterRight >= 4 || winCounterLeft >= 4){
+      `Was the game won?` = true
+    }
+    `Was the game won?`
+  }
+
   def areFourConnected(player: String): Boolean = {
-    val winNumber = 4
 
-    def horizontalCheck(): Boolean ={
-      var winCounter = 0
-      var `Was the game won?` = false
 
-      for(currentRow <- 0 until row) for(currentColumn <- 0 until column){
-        if(matrix(currentRow)(currentColumn) == player) winCounter += 1 else winCounter = 0
-        if(winCounter == winNumber){`Was the game won?` = true}
-      }
-      `Was the game won?`
-    }
-
-    def verticalCheck(): Boolean ={
-      var winCounter = 0
-      var `Was the game won?` = false
-      for(currentColumn <- 0 until column) for(currentRow <- 0 until row){
-        if(matrix(currentRow)(currentColumn) == player) winCounter += 1 else winCounter = 0
-        if(winCounter == winNumber){`Was the game won?` = true}
-      }
-      `Was the game won?`
-    }
-
-    if(horizontalCheck() || verticalCheck()) true else false
+    if(horizontalCheck(player) || verticalCheck(player) || diagonalCheck(player)) true else false
 
   }
 
   def setElementinMatchfield(row:Int, column:Int, newValue:String): Boolean ={
+    lastMoveRow = row
+    lastMoveColumn = column
     if(matrix(row)(column) == "empty") {
       try{
         if (matrix(row + 1)(column) != "empty") {
